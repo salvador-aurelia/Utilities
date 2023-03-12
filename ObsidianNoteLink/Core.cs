3f109b2ce5e00f0a90ceab95abcb7878a6316bb4
salvador-aurelia <salvador-aurelia@users.noreply.github.com>
2023-03-11 22:19:08 -0500

    Version 0.1.000
    Working Prototype.
    Hardcoded PathNames for the strings.
    Next step is use arguments or ask for input for path information.

---
+++ ObsidianNoteLink/Core.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ObsidianNoteLink;
+
+public class Core
+{
+    static void Main(string[] args)
+    {
+        
+        string sourcePath = @"C:\Users\SalboPC\Documents\Godot Games";
+        string destinationPath = @"C:\Users\SalboPC\Documents\dev\DevNotes";
+        
+        var csFiles = Directory.GetFiles(sourcePath, "*.cs", SearchOption.AllDirectories);
+        
+        var noteRegex = new Regex(@"\[\s*Note\s*\]\s*(?<note>[\s\S]*?)\*/", RegexOptions.Multiline);
+        
+        var comments = from file in csFiles
+            let projectName = Path.GetFileName(Path.GetDirectoryName(file))
+            //let projectPathName = Path.GetFullPath(file)
+            let projectPathName = Path.Combine(destinationPath, Path.GetRelativePath(sourcePath, Path.GetDirectoryName(file)))
+                let className = Path.GetFileNameWithoutExtension(file)
+                let fileText = File.ReadAllText(file)
+                let matches = noteRegex.Matches(fileText)
+                
+                where matches.Count > 0
+                let noteText = string.Join(Environment.NewLine, matches.Cast<Match>().Select(m => m.Groups["note"].Value.Trim()))
+                group noteText by new {ProjectName = projectPathName, ClassName = className} into g
+                select new {g.Key.ProjectName, g.Key.ClassName, NoteText = string.Join(Environment.NewLine, g.ToArray())};
+
+        foreach (var comment in comments)
+        {
+            //string projectPath = Path.Combine(destinationPath, Path.GetFileName(Path.GetDirectoryName(comment.ProjectName)) ?? string.Empty);
+            Console.WriteLine("Finding comments in: " + comment.ClassName + "...");
+            string projectPath = Path.Combine(destinationPath, comment.ProjectName);
+            Console.WriteLine("Generating directory: " + projectPath + "...");
+            
+            if (!Directory.Exists(projectPath))
+            {
+                Console.WriteLine("Creating directory: " + projectPath + "...");
+                Directory.CreateDirectory(projectPath);
+            }
+
+            string classPath = Path.Combine(projectPath, comment.ClassName + ".md");
+            Console.WriteLine("Writing file: " + classPath + "...");
+            File.WriteAllText(classPath, comment.NoteText);
+        }
+                
+    }
+}
+
